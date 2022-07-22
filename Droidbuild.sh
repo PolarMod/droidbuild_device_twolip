9603f88d7c63f9d6c7bb5d78e21fef35b7dacafd
Andrewerr <toddot@protonmail.com>
2022-07-23 00:43:40 +0300

    [twolip] [manifests]: update revisions

--- Droidbuild.sh
+++ Droidbuild.sh
@@ -4,14 +4,14 @@ target_twolip(){
   LOCAL_PATH=$(dirname $BASH_SOURCE)
   include $BASEDIR/droidbuild/build_vars.sh
   TARGET_NEEDS_RESYNC=true
-  TARGET_LOCAL_MANIFESTS+=$LOCAL_PATH/manifests/twolip.xml
+  TARGET_LOCAL_MANIFESTS+=($LOCAL_PATH/manifests/twolip.xml)
   TARGET_ARCH_BITNESS=64
   TARGET_CODENAME="twolip"
   TARGET_FULLNAME="Xiaomi Redmi Note 6 Pro"
-  TARGET_BUILDTYPE="user"
+  TARGET_BUILDTYPE="eng"
   TARGET_SIGNED_BUILD=true
   TARGET_HOSTS_CAT=2
-  TARGET_USES_MICROG=1
+  TARGET_USES_MICROG=0
   target_build-device
 }
 
